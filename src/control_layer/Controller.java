695cbe51ece307ed282a1715c9a0e36f0fc8a48e
august-sosick-ENSF <august-sosick@users.noreply.github.com>
2020-12-01 17:39:18 -0700

    Update

--- src/control_layer/Controller.java
+++ src/control_layer/Controller.java
@@ -18,21 +18,31 @@ import presentation_layer.PaymentView;
 import presentation_layer.ReservationView;
 
 import java.awt.CardLayout;
+import java.security.Security;
 import java.sql.SQLException;
 //Library import
 import java.util.ArrayList;
 import java.util.Calendar;
+import java.util.Date;
 import java.util.HashMap;
 import java.util.Iterator;
 import java.util.Map;
+import java.util.Properties;
 import java.util.Map.Entry;
 import java.util.Timer;
 import java.util.TimerTask;
 
+import javax.mail.Message;
 import javax.mail.MessagingException;
+import javax.mail.Session;
+import javax.mail.internet.AddressException;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
 import javax.swing.JFrame;
 import javax.swing.JOptionPane;
 import javax.swing.JPanel;
+
+import com.sun.mail.smtp.SMTPTransport;
 
 public class Controller {
 	private HashMap<Integer, Theatre> theaterList;
@@ -62,7 +72,7 @@ public class Controller {
 		
 		//Testing Email
 		try {
-			Email.Send("ensf480finalprojectemail", "ensfpassword1&", "ensf480finalprojectemail@gmail.com"
+		  Send("ensf480finalprojectemail", "ensfpassword1&", "ensf480finalprojectemail@gmail.com", ""
 					, "Whats good", "Test");
 		} catch (MessagingException e) {
 			// TODO Auto-generated catch block
@@ -70,6 +80,58 @@ public class Controller {
 		}
 		
 	}
+	
+	/**
+	 * CODE FOR THIS SECTION BASED ON doraemon's stackoverflow post seen here:
+	 * @source https://stackoverflow.com/questions/3649014/send-email-using-java
+	 * 
+	 */
+    public void Send(final String username, final String password, String recipientEmail, String ccEmail, String title, String message) throws AddressException, MessagingException {
+        Security.addProvider(new com.sun.net.ssl.internal.ssl.Provider());
+        final String SSL_FACTORY = "javax.net.ssl.SSLSocketFactory";
+
+        // Get a Properties object
+        Properties props = System.getProperties();
+        props.setProperty("mail.smtps.host", "smtp.gmail.com");
+        props.setProperty("mail.smtp.socketFactory.class", SSL_FACTORY);
+        props.setProperty("mail.smtp.socketFactory.fallback", "false");
+        props.setProperty("mail.smtp.port", "465");
+        props.setProperty("mail.smtp.socketFactory.port", "465");
+        props.setProperty("mail.smtps.auth", "true");
+
+        /*
+        If set to false, the QUIT command is sent and the connection is immediately closed. If set 
+        to true (the default), causes the transport to wait for the response to the QUIT command.
+
+        ref :   http://java.sun.com/products/javamail/javadocs/com/sun/mail/smtp/package-summary.html
+                http://forum.java.sun.com/thread.jspa?threadID=5205249
+                smtpsend.java - demo program from javamail
+        */
+        props.put("mail.smtps.quitwait", "false");
+
+        Session session = Session.getInstance(props, null);
+
+        // -- Create a new message --
+        final MimeMessage msg = new MimeMessage(session);
+
+        // -- Set the FROM and TO fields --
+        msg.setFrom(new InternetAddress(username + "@gmail.com"));
+        msg.setRecipients(Message.RecipientType.TO, InternetAddress.parse(recipientEmail, false));
+
+        if (ccEmail.length() > 0) {
+            msg.setRecipients(Message.RecipientType.CC, InternetAddress.parse(ccEmail, false));
+        }
+
+        msg.setSubject(title);
+        msg.setText(message, "utf-8");
+        msg.setSentDate(new Date());
+
+        SMTPTransport t = (SMTPTransport)session.getTransport("smtps");
+
+        t.connect("smtp.gmail.com", username, password);
+        t.sendMessage(msg, msg.getAllRecipients());      
+        t.close();
+    }
 
 	/*
 	 * Load all information about theater, movie to java object.
